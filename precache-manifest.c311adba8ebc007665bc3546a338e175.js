b7a75b39a6c3d75691528496aeca574ace179b7c
Nahuelsan <nahuel.-sanchez@live.com.ar>
2020-11-21 22:02:55 -0300

    Updates

---
+++ precache-manifest.c311adba8ebc007665bc3546a338e175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3e5f4d30bcf33b8f46ca867dfc570e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "526612d22a4cc2c4c498",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526612d22a4cc2c4c498",
+    "url": "/static/js/main.82b1ee0d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
